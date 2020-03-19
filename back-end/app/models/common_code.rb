f97a6eeb8cbcaa0cc8bba2c075ff6446fc4c03e3
hjh <bhnall>
2020-03-19 21:28:44 +0900

    test common_code select

--- back-end/app/models/common_code.rb
+++ back-end/app/models/common_code.rb
@@ -1,2 +1,14 @@
 class CommonCode < ApplicationRecord
+
+    def CommonCode.getMainCategories
+        mainCategory = CommonCode.where('"codeTypeId"= 1')
+    end
+
+    def CommonCode.getMiddleCategories(param)
+        middleCategory = CommonCode.where('"codeTypeId"= 2 and "parentId"= :id', id: param[:mainId]).order('name asc')
+    end
+
+    def CommonCode.getSubCategories(param)
+        subCategory = CommonCode.where('"codeTypeId"= 3 and "parentId"= :id', id: param[:middleId]).order('name asc')
+    end
 end
--- back-end/test/models/common_code_test.rb
+++ back-end/test/models/common_code_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CommonCodeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Get All Main Category" do
+    mainCategory = CommonCode.getMainCategories
+
+    assert_equal 3, mainCategory.size
+    assert_equal '금융', mainCategory[0].name
+  end
+
+  test "Get Middle Category By Param" do
+    param = {mainId: 1}
+    middleCategory = CommonCode.getMiddleCategories(param)
+
+    assert_equal 3, middleCategory.size
+  end
+
+  test "Get Sub Category By Param" do
+    param = {middleId: 4}
+    subCategory = CommonCode.getSubCategories(param)
+
+    assert_equal 2, subCategory.size
+  end
+
 end
