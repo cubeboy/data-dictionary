bc93a1e0930e3f5fb5fee8f6de318538c1366240
kim-juhan <prototype01@naver.com>
2020-03-24 14:51:26 +0900

    create word controller

--- back-end/app/controllers/word_controller.rb
+++ back-end/app/controllers/word_controller.rb
@@ -1,2 +1,61 @@
 class WordController < ApplicationController
+    def index
+        words = []
+        if(params)
+            begin
+                wordParam = JSON.parse(params, {symbolize_names: true})
+            rescue
+                wordParam = params
+            end
+            words = Word.getWords wordParam
+        else
+            words = Word.all
+        end
+        render json: words.to_json
+    end
+
+    def show
+        word = Word.find(params[:id])
+
+        render json: word.to_json
+    end
+
+
+    def new
+        word = Word.new
+    end
+
+    def create
+        word = Word.new(word_params)
+
+        # if word.save
+        #     redirect_to word
+        # else
+        #     render 'new'
+        # end
+    end
+
+    def update
+        word = Word.find(params[:id])
+
+        # if word.update(word_params)
+        #     redirect_to word
+        # else
+        #     render 'edit'
+        # end
+    end
+
+    def destroy
+        word = Word.find(params[:id])
+        word.destroy
+
+        # redirect_to word_path
+    end
+
+    private
+        def word_params
+            params.require(:word).permit(:name, :engName)
+        end
+
+
 end
--- back-end/test/controllers/word_controller_test.rb
+++ back-end/test/controllers/word_controller_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class WordControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  url = '/word'
+  searchKorName = "계좌"
+  searchEncName = "thdr"
+  searchId = 3
+
+  test "Word Controller Search all test" do
+    assert_generates url, {controller: url, action: 'index'}
+    getWord = get url
+    assert_equal 200, getWord, "Word Controller Search all get url Error"
+    assert_response :success
+    words = JSON.parse(@response.body)
+    assert 0 < words.size, "Word Controller Search all no data"
+  end
+
+  test "Word Controller method getWords wordParam test" do
+    getWord = get url, params: {search: searchKorName}
+    assert_equal 200, getWord, "Word Controller method getWords wordParam get url searchKorName Error"
+    assert_response :success
+    words = JSON.parse(@response.body)
+    words.each do |word|
+      assert (word['name'].upcase.include? searchKorName.upcase) || (word['engName'].upcase.include? searchKorName.upcase), 'Word Controller method getWords wordParam - searchKorName Fail'
+    end
+
+    getWord = get url, params: {search: searchEncName}
+    assert_equal 200, getWord, "Word Controller method getWords wordParam url searchEncName Error"
+    assert_response :success
+    words = JSON.parse(@response.body)
+    words.each do |word|
+      assert (word['name'].upcase.include? searchEncName.upcase) || (word['engName'].upcase.include? searchEncName.upcase), 'Word Controller method getWords wordParam - searchEncName Fail'
+    end
+  end
+
+  test "Word Controller Search find id test" do
+    getWord = get url + "/#{searchId}"
+    assert_equal 200, getWord, "Word Controller Search find id - get url Error"
+    assert_response :success
+    word = JSON.parse(@response.body)
+    assert_equal searchId, word["id"], "Word Controller Search find id - id Fail"
+  end
+
 end
