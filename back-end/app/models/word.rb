fe03b0d8c7f61a3c4a9e3d0cee8ade4d9f4a2811
kim-juhan <prototype01@naver.com>
2020-03-24 13:39:24 +0900

    create word.rb and word_test.rb

--- back-end/app/models/word.rb
+++ back-end/app/models/word.rb
@@ -1,2 +1,12 @@
 class Word < ApplicationRecord
+    validates :name, :engName, :shortEng, :entity, :column, :javascript, :wordClass, :wordClassMember, :paramValue , presence:true, allow_blank: false, allow_nil: false
+
+    has_many :termsWords, :foreign_key => 'wordId'
+    has_many :terms, :through => :termsWords
+
+    def Word.getWords wordParam
+        likeWord = "%#{wordParam[:search]}%"
+        word = Word.where('(name ilike :search or "engName" ilike :search)', search: likeWord)
+    end
+
 end
--- back-end/test/models/word_test.rb
+++ back-end/test/models/word_test.rb
@@ -1,7 +1,213 @@
 require 'test_helper'
 
 class WordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  searchKorName = "계좌"
+  searchEncName = "thdr"
+  searchId = 2
+
+  test "save blank, nil vaildates" do
+    word = Word.new({
+      name: '',
+      engName: 'Account',
+      shortEng: 'Acc',
+      entity: 'T_Acc',
+      column: 'acc',
+      javascript: 'acc',
+      wordClass: 'Acc',
+      wordClassMember: 'acc',
+      paramValue: 'acc',
+    })
+    assert !word.save, 'Save Validates word name Fail'
+    assert_equal 1, word.errors.size , 'Validates Fail - name'
+
+    word = Word.new({
+      name: '계좌',
+      engName: '',
+      shortEng: 'Acc',
+      entity: 'T_Acc',
+      column: 'acc',
+      javascript: 'acc',
+      wordClass: 'Acc',
+      wordClassMember: 'acc',
+      paramValue: 'acc',
+    })
+    assert !word.save, 'Save Validates word engName Fail'
+    assert_equal 1, word.errors.size , 'Validates Fail - engName'
+
+    word = Word.new({
+      name: '계좌',
+      engName: 'Account',
+      shortEng: '',
+      entity: 'T_Acc',
+      column: 'acc',
+      javascript: 'acc',
+      wordClass: 'Acc',
+      wordClassMember: 'acc',
+      paramValue: 'acc',
+    })
+    assert !word.save, 'Save Validates word shortEng Fail'
+    assert_equal 1, word.errors.size , 'Validates Fail - shortEng'
+
+    word = Word.new({
+      name: '계좌',
+      engName: 'Account',
+      shortEng: 'Acc',
+      entity: '',
+      column: 'acc',
+      javascript: 'acc',
+      wordClass: 'Acc',
+      wordClassMember: 'acc',
+      paramValue: 'acc',
+    })
+    assert !word.save, 'Save Validates word entity Fail'
+    assert_equal 1, word.errors.size , 'Validates Fail - entity'
+
+    word = Word.new({
+      name: '계좌',
+      engName: 'Account',
+      shortEng: 'Acc',
+      entity: 'T_Acc',
+      javascript: 'acc',
+      wordClass: 'Acc',
+      wordClassMember: 'acc',
+      paramValue: 'acc',
+    })
+    assert !word.save, 'Save Validates word column Fail'
+    assert_equal 1, word.errors.size , 'Validates Fail - column'
+
+    word = Word.new({
+      name: '계좌',
+      engName: 'Account',
+      shortEng: 'Acc',
+      entity: 'T_Acc',
+      column: 'acc',
+      javascript: '',
+      wordClass: 'Acc',
+      wordClassMember: 'acc',
+      paramValue: 'acc',
+    })
+    assert !word.save, 'Save Validates word javascript Fail'
+    assert_equal 1, word.errors.size , 'Validates Fail - javascript'
+
+    word = Word.new({
+      name: '계좌',
+      engName: 'Account',
+      shortEng: 'Acc',
+      entity: 'T_Acc',
+      column: 'acc',
+      javascript: 'acc',
+      wordClass: '',
+      wordClassMember: 'acc',
+      paramValue: 'acc',
+    })
+    assert !word.save, 'Save Validates wordClass javascript Fail'
+    assert_equal 1, word.errors.size , 'Validates Fail - wordClass'
+
+    word = Word.new({
+      name: '계좌',
+      engName: 'Account',
+      shortEng: 'Acc',
+      entity: 'T_Acc',
+      column: 'acc',
+      javascript: 'acc',
+      wordClass: 'Acc',
+      wordClassMember: '',
+      paramValue: 'acc',
+    })
+    assert !word.save, 'Save Validates wordClassMember javascript Fail'
+    assert_equal 1, word.errors.size , 'Validates Fail - wordClassMember'
+
+    word = Word.new({
+      name: '계좌',
+      engName: 'Account',
+      shortEng: 'Acc',
+      entity: 'T_Acc',
+      column: 'acc',
+      javascript: 'acc',
+      wordClass: 'Acc',
+      wordClassMember: 'acc',
+      paramValue: '',
+    })
+    assert !word.save, 'Save Validates paramValue javascript Fail'
+    assert_equal 1, word.errors.size , 'Validates Fail - paramValue'
+  end
+
+  test "Save Word Data" do
+    wordParam = Word.new({
+      id: 9999999,
+      name: '계좌',
+      engName: 'Account',
+      shortEng: 'Acc',
+      entity: 'T_Acc',
+      column: 'acc',
+      javascript: 'acc',
+      wordClass: 'Acc',
+      wordClassMember: 'acc',
+      paramValue: 'acc',
+    })
+    assert wordParam.save, 'Save Fail'
+    assert_equal 0, wordParam.errors.size, 'Save word Fail'
+    word = Word.find(9999999)
+    assert_equal wordParam.name, word.name, 'Save word Fail - Word name error'
+    assert_equal wordParam.engName, word.engName, 'Save word Fail - Word engName error'
+    assert_equal wordParam.shortEng, word.shortEng, 'Save word Fail - Word shortEng error'
+    assert_equal wordParam.entity, word.entity, 'Save word Fail - Word entity error'
+    assert_equal wordParam.column, word.column, 'Save word Fail - Word column error'
+    assert_equal wordParam.javascript, word.javascript, 'Save word Fail - Word javascript error'
+    assert_equal wordParam.wordClass, word.wordClass, 'Save word Fail - Word wordClass error'
+    assert_equal wordParam.wordClassMember, word.wordClassMember, 'Save word Fail - Word wordClassMember error'
+    assert_equal wordParam.paramValue, word.paramValue, 'Save word Fail - Word paramValue error'
+  end
+
+  test "Update Word Data" do
+    wordUpdateData = Word.find(searchId)
+    assert !(searchKorName == wordUpdateData.name), 'Update Word - 업데이트전 name 값이 틀림'
+    assert !(searchEncName == wordUpdateData.engName), 'Update Word - 업데이트전 engName 값이 틀림'
+    wordUpdateData.name = searchKorName
+    wordUpdateData.engName = searchEncName
+    wordUpdateData.save
+    assert wordUpdateData.save, 'Update Fail'
+    assert_equal 0, wordUpdateData.errors.size, 'Update Word Fail'
+
+    word = Word.find(searchId)
+    assert_equal searchKorName, word.name, 'Update Word - Word name Fail'
+    assert_equal searchEncName, word.engName, 'Update Word - Word engName Fail'
+  end
+
+  test "Word destroy Test" do
+    word = Word.find(searchId)
+    assert_equal searchId, word[:id], 'Word destroy - id Fail'
+    word.destroy
+    assert_equal false, Word.exists?(searchId), 'Word destroy - destroy Fail'
+  end
+
+  test "Search Word all" do
+    words = Word.all
+    assert words.size > 0, 'Search Word all - searchKorName data nil'
+    assert_equal Word.all.size, words.size, 'Search Word all - searchKorName size Fail'
+
+    words = Word.all
+    assert words.size > 0, 'Search Word all - searchEncName data nil'
+    assert_equal Word.all.size, words.size, 'Search Word all - searchEncName size Fail'
+  end
+
+  test "Search Word method getWords wordParam" do
+    wordParam = {search: searchKorName}
+    words = Word.getWords wordParam
+    words.each do |word|
+      assert (word.name.upcase.include? searchKorName.upcase) || (word.engName.upcase.include? searchKorName.upcase), 'Search Word method getWords wordParam - searchKorName Fail'
+    end
+
+    wordParam = {search: searchEncName}
+    words = Word.getWords wordParam
+    words.each do |word|
+      assert (word.name.upcase.include? searchEncName.upcase) || (word.engName.upcase.include? searchEncName.upcase), 'Search Word method getWords wordParam - searchEncName Fail'
+    end
+  end
+
+  test "Search find id" do
+    word = Word.find(searchId)
+    assert_equal searchId, word[:id], 'Search find id - id Fail'
+  end
+
 end
