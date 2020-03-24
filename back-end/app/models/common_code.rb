5d0693c8bc9ed420a58955e95c54034120bf5e70
hjh <bhnall>
2020-03-24 09:59:38 +0900

    add validates at common_code & common_code_type

--- back-end/app/models/common_code.rb
+++ back-end/app/models/common_code.rb
@@ -1,5 +1,6 @@
 class CommonCode < ApplicationRecord
-    belongs_to :commonCodeType
+    validates :name, :code_type_id, :eng_name, :parent_id, presence: true, allow_blink: false, allow_nil: false
+    belongs_to :common_code_type
 
     def CommonCode.getCategoriesByParams(params)
         categories = CommonCode.where('"codeTypeId"= :codeTypeId and "parentId" = :parentId',
--- back-end/app/models/common_code_type.rb
+++ back-end/app/models/common_code_type.rb
@@ -1,11 +1,11 @@
 class CommonCodeType < ApplicationRecord
-    
+    validates :name, :eng_name, presence: true, allow_blink: false, allow_nil: false
     # has_many :commonCodes, foreign_key => "codeTypeId"
 
     # def CommonCodeType.getAllCodeTypes
     #     codeTypes = CommonCodeType.select('id','name','engName').joins(:commonCodes)
     # end
-    
+
     has_many :commoncodes
 
     def CommonCodeType.getAllCodeTypes
