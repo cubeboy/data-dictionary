0d3d53ac154ef1d5e79f0801d391ecb1baf3cc02
hjh <bhnall>
2020-03-25 15:49:31 +0900

    page move when enter key tabed

--- front-end/src/components/SearchCard.vue
+++ front-end/src/components/SearchCard.vue
@@ -3,8 +3,8 @@
   <div>
     <div style="text-align:center" id="filter">
       <Category/>
-      <div>
-        <input v-model="word" placeholder="Search Here">
+      <div class="row">
+        <v-text-field v-model="word" label="Search" outlined v-on:keyup.enter='submit()'></v-text-field>
         <router-link :to=" '/common_code/' + word"><v-chip class="ma-2">Search</v-chip></router-link>
       </div>
     </div>
@@ -35,6 +35,9 @@ export default {
   methods: {
     conveySelectedItem (selectedItem) {
       console.log('parent>')
+    },
+    submit () {
+      location.href = 'common_code/' + this.word
     }
   }
 }
